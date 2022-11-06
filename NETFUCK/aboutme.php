44728aa1eacf07f975244816f81e2335ad80579e
Pd011161 <78379979+Pd011161@users.noreply.github.com>
2022-11-07 01:23:04 +0700

    Add files via upload

---
+++ NETFUCK/aboutme.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABOUT ME</title>
+    <link rel="stylesheet" href="./css/style-about.css">
+</head>
+<body>
+    
+    <header>
+        <a href="#" class="logo">
+            <i class='bx bxs-movie-play'></i>
+            <h3 class='NF'>NETFUCK</h3>
+        </a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <!-- MENU -->
+        <ul class="navbar">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="index.php#silver">SILVER</a></li>
+            <li><a href="index.php#gold">GOLD</a></li>
+            <li><a href="index.php#diamond">DIAMOND</a></li>
+            <li><a href="#aboutme" class="home-action">ABOUT ME</a></li>
+
+        </ul>
+        <a href="login.php" class="btn">SIGN IN</a>
+    </header>
+
+    <div class="text">PDev</div>
+    
+    <div class="card">
+        <div class="imgBx">
+            <img src="./img/imgPd.JPG">
+        </div>
+        <div class="content">
+            <div class="details">
+
+                <h2>Predee Chaiyakit
+                    <br>
+                    <span>
+                        Senior Developer
+                    </span>
+                </h2>
+
+                <div class="data">
+                    <h3>277<br><span>Experience</span></h3>
+                    <h3>3<br><span>Project</span></h3>
+                    <h3>22<br><span>Age</span></h3>
+                </div>
+
+                <div class="Btn">
+                    <button><a href="https://www.instagram.com/pdev_official" target="_blank">Follow</a></button>
+                    <button><a href="https://line.me/ti/p/MrFuA62DEJ" target="_blank">Message</a></button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
